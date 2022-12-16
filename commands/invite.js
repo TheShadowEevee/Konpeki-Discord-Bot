25370e9dc1f326f53f81307cb58f2502f065754a
Michael <TheShadowEeveeYT@gmail.com>
2022-12-16 13:16:54 -0500

    Add an invite command to share bot invite link

---
+++ commands/invite.js
@@ -0,0 +1,20 @@
+/*
+*  Konpeki Discord Bot - Slash Command Definition File
+*  invite.js - Uses your bot's clientID to create and share it's own invite link
+*
+*  Function modified from https://discordjs.guide/creating-your-bot/slash-commands.html
+*/
+
+const { SlashCommandBuilder } = require('discord.js');
+
+// Get the clientID and bot name from the config file
+const { botName, clientId } = require('../config.json');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('invite')
+		.setDescription(`Invite ${botName} to your own server!`),
+	async execute(interaction) {
+		await interaction.reply({ content: `Use this link to invite ${botName} (That's me!) to your own server!\nhttps://discord.com/oauth2/authorize?client_id=${clientId}&permissions=274877908992&scope=bot%20applications.commands`, ephemeral: true });
+	},
+};
